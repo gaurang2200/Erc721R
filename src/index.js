8800b779026cfffb609a7141ea2be859ec852910
gaurang2200 <guptakgk14@gmail.com>
2022-03-26 02:49:16 +0530

    Header UI Completed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: 'Poppins',
+      color: 'white'
+    },
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
