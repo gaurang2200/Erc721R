2839316efb62e4b217394d29ce0fc7ad01bf3fbd
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-03-31 13:12:36 +0530

    Designed edited and SEO tags inclueded

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,19 +5,19 @@ import gradientBg from '../images/gradientBg.svg'
 
 const HeroSection = () => {
   return (
-    <div>
+    <>
       <div className="relative flex flex-col items-center justify-center mx-4 md:mx-12 lg:mx-20 pb-8 overflow-hidden">
         <img src={heroBg} className="absolute h-full w-full -z-10" />
         <img src={gradientBg} className="absolute h-full w-full -z-10" />
         <img src={heroImage} className="-z-10"/>
         <div className="z-10 text-white flex flex-col items-center">
-          <p className="text-6xl text-center font-bold mt-12 mb-4">
+          <p className="text-6xl opacity-100 text-center font-bold mt-12 mb-4">
             ERC721R
           </p>
-          <p className="text-5xl text-center font-bold">
+          <p className="text-5xl opacity-90 text-center font-bold">
             A new way of launching NFT collections
           </p>
-          <p className="text-center my-12">
+          <p className="text-center opacity-100 my-12">
             ERC271R is a sprint nft launcher contract through which you can launch a refundable NFT
             <br /> collection and also reward the early minters
           </p>
@@ -26,7 +26,7 @@ const HeroSection = () => {
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
